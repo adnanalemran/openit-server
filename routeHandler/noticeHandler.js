b2e43eb83e5cde74ca1f81e93e6e8bcc6f2940d7
adnanalemran <adnanalemranontor@gmail.com>
2024-03-24 01:27:32 +0600

    delete api added

--- routeHandler/noticeHandler.js
+++ routeHandler/noticeHandler.js
@@ -4,20 +4,23 @@ const mongoose = require("mongoose");
 const noticeCollection = require("../schemas/noticeSchemas");
 require("dotenv").config();
 
-
-
 //ok
 router.get("/", async (req, res) => {
   const notice = await noticeCollection.find();
   res.send(notice);
 });
 
-
 //ok
 router.post("/", async (req, res) => {
   const notice = req.body;
   const create = await noticeCollection.create(notice);
   res.send(create);
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await noticeCollection.deleteOne({ _id: id });
+  res.send(result);
 });
 
 module.exports = router;
